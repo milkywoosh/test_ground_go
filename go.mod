69d2f0282bc649b9078d7bff133306310df7a41f
lukmanx <archer.lukman@gmail.com>
2022-03-28 23:28:10 +0700

    - test dependency from GOMOD_BASIC_MATH

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/milkywoosh/test_ground_go
 
 go 1.17
+
+require github.com/milkywoosh/gomod_basic_math v1.0.3 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"fmt"
+
+	"github.com/milkywoosh/gomod_basic_math"
+)
+
+func main() {
+	result := gomod_basic_math.Math_modulo(10, 2)
+	fmt.Println(result)
+
+}
