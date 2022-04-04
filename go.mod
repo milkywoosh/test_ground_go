b23671123cd4dce593f22885ff6c0ec495712f74
lukmanx <archer.lukman@gmail.com>
2022-04-04 22:23:36 +0700

    re learn struct dtype in golang

--- go.mod
+++ go.mod
@@ -2,7 +2,8 @@ module github.com/milkywoosh/test_ground_go
 
 go 1.17
 
-// github.com/milkywoosh/gomod_basic_math v1.0.3 // indirect
-// github.com/milkywoosh/gomod_slice v1.0.0 // indirect
-
-require github.com/milkywoosh/gomod_struct_modif v1.0.0
+require (
+	// _ github.com/milkywoosh/gomod_basic_math v1.0.3 // indirect
+	// _ github.com/milkywoosh/gomod_slice v1.0.0 // indirect
+	// _ github.com/milkywoosh/gomod_struct_modif v1.0.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"github.com/milkywoosh/gomod_struct_modif"
+	"fmt"
+	// _ "github.com/milkywoosh/gomod_slice"
+	// _ "github.com/milkywoosh/gomod_struct_modif"
 )
 
-func main() {
-	data := gomod_struct_modif.TestStruct{}
-	data.SetName("ben")
-	data.SetWealth(100000)
-	data.ShowData()
+type Humandroid struct {
+	id        int
+	name      string
+	component map[string]string
+	ability   []string
+}
 
+func main() {
+	prototype1 := Humandroid{}
+	prototype1.id = 1
+	prototype1.name = "franky"
+	prototype1.component = map[string]string{"tes": "tes1"}
+	prototype1.ability = []string{"speak", "count", "fly"}
+
+	fmt.Println(prototype1)
 }
